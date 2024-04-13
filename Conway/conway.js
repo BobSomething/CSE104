1e255b1e90b268fa05ab2fb7d7186fe28d39fdf5
BobSomething <61118026+BobSomething@users.noreply.github.com>
2024-04-13 22:22:32 +0200

    a

--- Conway/conway.js
+++ Conway/conway.js
@@ -7,5 +7,6 @@ const board = document.querySelector('#board')
 for(let i=0; i<width; i++) {
     const newdiv = document.createElement('div')
     newdiv.classList.add('cell')
+    console.log(1)
     board.appendChild(newdiv);
 }
