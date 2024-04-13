42cc2c4771bfbec3aee232720e6f35e263c23886
BobSomething <61118026+BobSomething@users.noreply.github.com>
2024-04-13 22:19:50 +0200

    22:19 13.04

--- Conway/conway.js
+++ Conway/conway.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let width = 12;
+let height = 12;
+const board = document.querySelector('#board')
+
+for(let i=0; i<width; i++) {
+    const newdiv = document.createElement('div.cell')
+    board.appendChild(newdiv);
+}
