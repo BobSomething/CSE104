43ead8726d9823b81df49888a445767b7a72d56a
BobSomething <61118026+BobSomething@users.noreply.github.com>
2024-04-13 22:57:18 +0200

    a

--- Conway/conway.js
+++ Conway/conway.js
@@ -3,6 +3,16 @@
 let width = 12;
 let height = 12;
 const board = document.querySelector('#board');
+const widthchange = document.querySelector('#width');
+const heightchange = document.querySelector('#height');
+
+widthchange.addEventListener('input', change);
+heightchange.addEventListener('input', change);
+
+function change(event) {
+    width = widthchange.value;
+    height = heightchange.value;
+}
 
 for(let i=0; i<width*height; i++) {
     const newdiv = document.createElement('div');
